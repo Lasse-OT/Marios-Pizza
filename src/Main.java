4c1d3ab7ebbce7dc035a4013d3e05310f2cecdab
simof <sifu0001@stud.kea.dk>
2024-04-09 12:58:04 +0200

    import java.util.*; import java.io.*;
    public class PizzaMenu {
        public static void main(String[] args) throws FileNotFoundException {
            Scanner scan = new Scanner(System.in);

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,6 @@
-hallo
+import java.util.*;
+import java.io.*;
+
+public class PizzaMenu {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(System.in);
