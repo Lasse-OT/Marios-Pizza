973c1880a804da5b158bf96faecbcab66f732570
simof <sifu0001@stud.kea.dk>
2024-04-09 13:06:31 +0200

    import java.util.*; import java.io.*;
    public class PizzaMenu {
        public static void main(String[] args) throws FileNotFoundException {
            Scanner scan = new Scanner(System.in);

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-public class PizzaMenu {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(System.in);
