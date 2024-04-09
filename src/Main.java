b3799838e05c5e7e568c69a91d385693bc26a768
lasse <laot0001@stud.kea.dk>
2024-04-09 13:03:35 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,5 @@ import java.io.*;
 public class PizzaMenu {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(System.in);
+
+        Test test test
